22a4073b71f719b1e6701bf96dff6c73ffea1de3
Kovid Goyal <kovid@kovidgoyal.net>
2016-06-30 21:23:02 +0530

    Add PyQt

---
+++ scripts/pkgs/pyqt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
+
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+import os
+
+from .constants import PYTHON, MAKEOPTS, build_dir, PREFIX
+from .utils import run
+
+
+def main(args):
+    b = build_dir()
+    lp = os.path.join(PREFIX, 'qt', 'lib')
+    run(PYTHON, 'configure.py', '--confirm-license', '--sip=%s/bin/sip' % PREFIX, '--qmake=%s/qt/bin/qmake' % PREFIX,
+        '--bindir=%s/bin' % b, '--destdir=%s/lib/python2.7/site-packages' % b, '--verbose', '--sipdir=%s/share/sip/PyQt5' % b,
+        '--no-stubs', '-c', '-j5', '--no-designer-plugin', '--no-qml-plugin', '--no-docstrings',
+        library_path=lp)
+    run('make ' + MAKEOPTS, library_path=lp)
+    run('make install')
