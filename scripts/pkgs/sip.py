1292f5432554346c6643e0e9cb4fb7a6540596ba
Kovid Goyal <kovid@kovidgoyal.net>
2016-06-30 21:21:19 +0530

    Forgot the includedir

--- scripts/pkgs/sip.py
+++ scripts/pkgs/sip.py
@@ -12,6 +12,6 @@ from .utils import run
 def main(args):
     b = build_dir()
     run(PYTHON, 'configure.py', '--bindir=%s/bin' % b, '--destdir=%s/lib/python2.7/site-packages' % b,
-        '--sipdir=%s/share/sip' % b, '--no-pyi', library_path=True)
+        '--sipdir=%s/share/sip' % b, '--incdir=%s/include/python2.7' % b, '--no-pyi', library_path=True)
     run('make ' + MAKEOPTS)
     run('make install')
