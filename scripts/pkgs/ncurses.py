036b2b6f32c1696f57b2da410f3f5d264d52c7b1
Kovid Goyal <kovid@kovidgoyal.net>
2016-06-30 22:49:16 +0530

    Fix non-root build of ncurses

--- scripts/pkgs/ncurses.py
+++ scripts/pkgs/ncurses.py
@@ -5,11 +5,12 @@
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 
+from .constants import PKG_CONFIG_PATH
 from .utils import simple_build
 
 
 def main(args):
-    simple_build('--with-shared --without-debug --without-ada --enable-widec --with-normal --enable-pc-files')
+    simple_build('--with-shared --without-debug --without-ada --enable-widec --with-normal --enable-pc-files --with-pkg-config-libdir=%s' % PKG_CONFIG_PATH)
 
 
 def filter_pkg(parts):
