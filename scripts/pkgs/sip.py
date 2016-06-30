10f49ca3e0ea8c244b00be0a3f079d1f8759e718
Kovid Goyal <kovid@kovidgoyal.net>
2016-06-30 20:49:20 +0530

    Add sip

---
+++ scripts/pkgs/sip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
+
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+from .constants import PYTHON, MAKEOPTS, build_dir
+from .utils import run
+
+
+def main(args):
+    b = build_dir()
+    run(PYTHON, 'configure.py', '--bindir=%s/bin' % b, '--destdir=%s/lib/python2.7/site-packages' % b,
+        '--sipdir=%s/share/sip' % b, '--no-pyi', library_path=True)
+    run('make ' + MAKEOPTS)
+    run('make install')
