d73b38c2d77c434cdd2bf7eed0d4684462d0926b
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-03 16:01:16 +0530

    Fix dbus connection to SystemBus failing

--- scripts/pkgs/dbus.py
+++ scripts/pkgs/dbus.py
@@ -6,13 +6,21 @@ from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 import os
 import re
+import shutil
 
-from .constants import build_dir, PREFIX
-from .utils import simple_build, replace_in_file
+from .constants import build_dir, PREFIX, MAKEOPTS
+from .utils import run, replace_in_file, install_binaries, copy_headers
 
 
 def main(args):
-    simple_build('--disable-dependency-tracking --disable-static '
-                 ' --disable-doxygen-docs --disable-xml-docs --disable-systemd --without-systemdsystemunitdir'
-                 ' --with-console-auth-dir=/run/console/ --disable-tests')
+    # dbus chooses where to look for config files based on values fed to
+    # ./configure, so we cannot configure to install it to prefix
+    run('./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-dependency-tracking --disable-static'
+        ' --disable-doxygen-docs --disable-xml-docs --disable-systemd --without-systemdsystemunitdir'
+        ' --with-console-auth-dir=/run/console/ --disable-tests')
+    run('make ' + MAKEOPTS)
+    install_binaries('dbus/.libs/libdbus*.so*')
+    os.makedirs(build_dir() + '/lib/pkgconfig')
+    shutil.copy2('dbus-1.pc', build_dir() + '/lib/pkgconfig')
+    copy_headers('dbus/*.h', destdir='include/dbus-1.0')
     replace_in_file(os.path.join(build_dir(), 'lib/pkgconfig/dbus-1.pc'), re.compile(br'^prefix=.+$', re.M), b'prefix=%s' % PREFIX)
