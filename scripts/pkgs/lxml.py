bce17fc83e6380bdb6984599426fd7d63f366ff7
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-03 09:54:20 +0530

    Fix the lxml pkg

--- scripts/pkgs/lxml.py
+++ scripts/pkgs/lxml.py
@@ -4,11 +4,15 @@
 
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
+import shutil
+import os
 
-from .constants import PREFIX, PYTHON
+from .constants import PREFIX, PYTHON, build_dir
 from .utils import python_build, run
 
 
 def main(args):
     run(PYTHON, *('setup.py build_ext -I {0}/include/libxml2 -L {0}/lib'.format(PREFIX).split()), library_path=True)
     python_build()
+    os.rename(os.path.join(build_dir(), 'sw/sw/lib'), os.path.join(build_dir(), 'lib'))
+    shutil.rmtree(os.path.join(build_dir(), 'sw'))
