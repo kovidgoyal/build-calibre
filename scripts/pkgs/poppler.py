2dbc353303f7385269465263e0367122ad2e65e0
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-08 12:02:02 +0530

    poppler for OS X

--- scripts/pkgs/poppler.py
+++ scripts/pkgs/poppler.py
@@ -6,8 +6,8 @@ from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 import os
 
-from .constants import CFLAGS, PREFIX
-from .utils import run, ModifiedEnv, install_binaries
+from .constants import CFLAGS, PREFIX, isosx, MAKEOPTS
+from .utils import run, ModifiedEnv, install_binaries, simple_build
 
 
 def main(args):
@@ -23,14 +23,17 @@ def main(args):
         LIBPNG_CFLAGS="-I{0}/include/libpng16".format(PREFIX),
         UTILS_LIBS="-lfreetype -lfontconfig -ljpeg -lpng"
     ):
-        run(('./configure --prefix={} '
-             '--without-x --enable-shared --disable-dependency-tracking  --disable-silent-rules '
-             '--enable-zlib --enable-splash-output --disable-cairo-output --disable-poppler-glib '
-             '--disable-poppler-qt4 --disable-poppler-qt5 --disable-poppler-cpp --disable-gtk-test '
-             '--enable-libjpeg --enable-compile-warnings=no').format(PREFIX))
-        for x in 'goo fofi splash poppler utils'.split():
-            os.chdir(x)
-            run('make')
-            os.chdir('..')
-        install_binaries('poppler/.libs/lib*.so*')
-        install_binaries('utils/.libs/pdf*', 'bin')
+        conf = ('--without-x --enable-shared --disable-dependency-tracking  --disable-silent-rules '
+                '--enable-zlib --enable-splash-output --disable-cairo-output --disable-poppler-glib '
+                '--disable-poppler-qt4 --disable-poppler-qt5 --disable-poppler-cpp --disable-gtk-test '
+                '--enable-libjpeg --enable-compile-warnings=no')
+        if isosx:
+            simple_build(conf)
+        else:
+            run(('./configure --prefix={} ' + conf).format(PREFIX))
+            for x in 'goo fofi splash poppler utils'.split():
+                os.chdir(x)
+                run('make ' + MAKEOPTS)
+                os.chdir('..')
+            install_binaries('poppler/.libs/lib*.so*')
+            install_binaries('utils/.libs/pdf*', 'bin')
