3d681e1094f9990cde30f5d174249190a2f2e4e4
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-08 11:41:17 +0530

    imobiledevice for OS X

--- scripts/pkgs/imobiledevice.py
+++ scripts/pkgs/imobiledevice.py
@@ -5,12 +5,18 @@
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 
-from .constants import PREFIX
+from .constants import PREFIX, LIBDIR
 from .utils import simple_build, ModifiedEnv
 
 
 def main(args):
     with ModifiedEnv(
+            libplist_CFLAGS="-I{}/include".format(PREFIX),
+            libplist_LIBS="-L{} -lplist".format(LIBDIR),
+            libplistmm_CFLAGS="-I{}/include".format(PREFIX),
+            libplistmm_LIBS="-L{} -lplist++".format(LIBDIR),
+            libusbmuxd_CFLAGS="-I{}/include".format(PREFIX),
+            libusbmuxd_LIBS="-L{} -lusbmuxd".format(LIBDIR),
             openssl_CFLAGS='-I%s/include' % PREFIX,
             openssl_LIBS='-lcrypto -lssl'
     ):
