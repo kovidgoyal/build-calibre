8fb906f28e51f68ed689ffbcb0bc47a1580fa104
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-08 11:38:04 +0530

    usbmuxd for OS X

--- scripts/pkgs/usbmuxd.py
+++ scripts/pkgs/usbmuxd.py
@@ -5,11 +5,16 @@
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 
-from .utils import simple_build, replace_in_file
+from .constants import PREFIX, LIBDIR
+from .utils import simple_build, replace_in_file, ModifiedEnv
 
 
 def main(args):
     # See http://www.mobileread.com/forums/showthread.php?t=255234 (prevent
     # detection of WiFi connected devices)
     replace_in_file('src/libusbmuxd.c', 'if (di) {', 'if (di && di->product_id != 0) {')
-    simple_build('--disable-dependency-tracking')
+    with ModifiedEnv(
+        libplist_CFLAGS="-I{}/include".format(PREFIX),
+        libplist_LIBS="-L{} -lplist".format(LIBDIR)
+    ):
+        simple_build('--disable-dependency-tracking')
