d57616ffe742b94bec279309325884e43a1d7243
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-08 11:30:59 +0530

    mozjpeg for OS X

--- scripts/pkgs/mozjpeg.py
+++ scripts/pkgs/mozjpeg.py
@@ -6,10 +6,12 @@ from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 import os
 
-from .constants import build_dir
+from .constants import build_dir, isosx, PREFIX
 from .utils import simple_build
 
 
 def main(args):
-    simple_build('--disable-dependency-tracking --disable-shared --with-jpeg8 --without-turbojpeg',
-                 override_prefix=os.path.join(build_dir(), 'private', 'mozjpeg'))
+    conf = '--disable-dependency-tracking --disable-shared --with-jpeg8 --without-turbojpeg'
+    if isosx:
+        conf += ' --host x86_64-apple-darwin NASM={}/bin/nasm'.format(PREFIX)
+    simple_build(conf, override_prefix=os.path.join(build_dir(), 'private', 'mozjpeg'))
