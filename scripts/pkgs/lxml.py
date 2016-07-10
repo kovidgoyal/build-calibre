e593d857e74c57c2f98ba7563459be957bd40aab
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-10 10:59:10 +0530

    lxml for OS X

--- scripts/pkgs/lxml.py
+++ scripts/pkgs/lxml.py
@@ -7,12 +7,13 @@ from __future__ import (unicode_literals, division, absolute_import,
 import shutil
 import os
 
-from .constants import PREFIX, PYTHON, build_dir
+from .constants import PREFIX, PYTHON, build_dir, isosx
 from .utils import python_build, run
 
 
 def main(args):
     run(PYTHON, *('setup.py build_ext -I {0}/include/libxml2 -L {0}/lib'.format(PREFIX).split()), library_path=True)
     python_build()
-    os.rename(os.path.join(build_dir(), 'sw/sw/lib'), os.path.join(build_dir(), 'lib'))
+    ddir = 'python' if isosx else 'lib'
+    os.rename(os.path.join(build_dir(), 'sw/sw/' + ddir), os.path.join(build_dir(), ddir))
     shutil.rmtree(os.path.join(build_dir(), 'sw'))
