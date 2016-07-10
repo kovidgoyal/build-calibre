3a6021616419003adeccc7d7b117a0914b1a2981
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-10 10:32:12 +0530

    PyQt for OS X

--- scripts/pkgs/pyqt.py
+++ scripts/pkgs/pyqt.py
@@ -6,16 +6,18 @@ from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 import os
 
-from .constants import PYTHON, MAKEOPTS, build_dir, PREFIX
+from .constants import PYTHON, MAKEOPTS, build_dir, PREFIX, isosx
 from .utils import run
 
 
 def main(args):
     b = build_dir()
     lp = os.path.join(PREFIX, 'qt', 'lib')
-    run(PYTHON, 'configure.py', '--confirm-license', '--sip=%s/bin/sip' % PREFIX, '--qmake=%s/qt/bin/qmake' % PREFIX,
-        '--bindir=%s/bin' % b, '--destdir=%s/lib/python2.7/site-packages' % b, '--verbose', '--sipdir=%s/share/sip/PyQt5' % b,
-        '--no-stubs', '-c', '-j5', '--no-designer-plugin', '--no-qml-plugin', '--no-docstrings',
-        library_path=lp)
+    cmd = [PYTHON, 'configure.py', '--confirm-license', '--sip=%s/bin/sip' % PREFIX, '--qmake=%s/qt/bin/qmake' % PREFIX,
+           '--bindir=%s/bin' % b, '--destdir=%s/lib/python2.7/site-packages' % b, '--verbose', '--sipdir=%s/share/sip/PyQt5' % b,
+           '--no-stubs', '-c', '-j5', '--no-designer-plugin', '--no-qml-plugin', '--no-docstrings']
+    if isosx:
+        cmd += ['--sip-incdir', os.path.join(PREFIX, 'include', 'python2.7')]
+    run(*cmd, library_path=lp)
     run('make ' + MAKEOPTS, library_path=lp)
     run('make install')
