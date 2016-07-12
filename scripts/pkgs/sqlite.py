d096d76bf3b1f4ff9313524c0923d861eace1236
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-12 18:21:26 +0530

    sqlite for windows

--- scripts/pkgs/sqlite.py
+++ scripts/pkgs/sqlite.py
@@ -5,11 +5,16 @@
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 
-from .constants import CFLAGS, isosx
-from .utils import simple_build, ModifiedEnv
+from .constants import CFLAGS, isosx, iswindows
+from .utils import simple_build, ModifiedEnv, copy_headers
 
 
 def main(args):
+    if iswindows:
+        # On windows we dont actually build sqlite as the python build script
+        # downloads its own version locked qslite. We just install the headers
+        copy_headers('sqlite3*.h')
+        return
     cflags = CFLAGS
     if isosx:
         cflags += ' -O2 -DSQLITE_ENABLE_LOCKING_STYLE'
