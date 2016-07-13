5f639a79fc21f551ece6bcc7aeead99adc323da6
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-13 11:21:41 +0530

    ICU for windows

--- scripts/pkgs/icu.py
+++ scripts/pkgs/icu.py
@@ -6,16 +6,28 @@ from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 import os
 import shutil
+import glob
 
-from .constants import build_dir, isosx, MAKEOPTS, LIBDIR
-from .utils import apply_patch, simple_build, install_binaries, run
+from .constants import build_dir, isosx, MAKEOPTS, LIBDIR, iswindows
+from .utils import apply_patch, simple_build, install_binaries, run, current_env, ModifiedEnv
 
 
 def main(args):
     os.chdir('source')
     # fix Malayalam encoding https://bugzilla.redhat.com/show_bug.cgi?id=654200
     apply_patch('icu.8198.revert.icu5431.patch', level=3, reverse=True)
-    if isosx:
+
+    if iswindows:
+        paths = current_env()['PATH'].split(os.pathsep)
+        paths.append('C:\\cygwin64\\bin')
+        with ModifiedEnv(PATH=os.pathsep.join(paths)):
+            run('C:/cygwin64/bin/dos2unix runConfigureICU')
+            run('C:/cygwin64/bin/bash ./runConfigureICU Cygwin/MSVC -prefix ' + build_dir().replace(os.sep, '/'))
+            run('C:/cygwin64/bin/make')  # parallel builds fail, so no MAKEOPTS
+            run('C:/cygwin64/bin/make install')
+            for dll in glob.glob(os.path.join(build_dir(), 'lib', '*.dll')):
+                os.rename(dll, os.path.join(build_dir(), 'bin', os.path.basename(dll)))
+    elif isosx:
         run('./runConfigureICU MacOSX --disable-samples --prefix=' + build_dir())
         run('make ' + MAKEOPTS)
         run('make install')
