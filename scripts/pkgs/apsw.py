d4a63fcfbb6b1f3cfef8ca35d60b5654aabb8420
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-14 16:14:41 +0530

    Oops

--- scripts/pkgs/apsw.py
+++ scripts/pkgs/apsw.py
@@ -4,11 +4,15 @@
 
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
+import os
+import shutil
 
-from .constants import iswindows, PYTHON, build_dir
+from .constants import iswindows, PYTHON, build_dir, SW, PREFIX
 from .utils import run
 
 
 if iswindows:
     def main(args):
         run(PYTHON, 'setup.py', 'fetch', '--all', '--missing-checksum-ok', 'build', 'install', '--root', build_dir())
+        os.rename(os.path.join(build_dir(), os.path.basename(SW), os.path.basename(PREFIX), 'private'), os.path.join(build_dir(), 'private'))
+        shutil.rmtree(os.path.join(build_dir(), os.path.basename(SW)))
