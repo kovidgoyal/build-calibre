35d8aa7cedf26a04a232bf86370426beb5cf0e8a
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-15 07:45:46 +0530

    sip for windows

--- scripts/pkgs/sip.py
+++ scripts/pkgs/sip.py
@@ -6,7 +6,7 @@ from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 import os
 
-from .constants import PYTHON, MAKEOPTS, build_dir, isosx, PREFIX
+from .constants import PYTHON, MAKEOPTS, build_dir, isosx, PREFIX, iswindows
 from .utils import run, replace_in_file
 
 
@@ -14,11 +14,20 @@ def main(args):
     b = build_dir()
     if isosx:
         b = os.path.join(b, 'python/Python.framework/Versions/2.7')
+    elif iswindows:
+        b = os.path.join(b, 'private', 'python')
     cmd = [PYTHON, 'configure.py', '--no-pyi', '--bindir=%s/bin' % build_dir()]
     cmd += ['--destdir=%s/lib/python2.7/site-packages' % b,
             '--sipdir=%s/share/sip' % b,
             '--incdir=%s/include/python2.7' % b]
     run(*cmd, library_path=True)
-    run('make ' + MAKEOPTS)
-    run('make install')
-    replace_in_file(os.path.join(b, 'lib/python2.7/site-packages/sipconfig.py'), build_dir(), PREFIX)
+    if iswindows:
+        run('nmake'), run('nmake install')
+    else:
+        run('make ' + MAKEOPTS)
+        run('make install')
+    q, r = build_dir(), PREFIX
+    if iswindows:
+        q = q.replace(os.sep, os.sep + os.sep)
+        r = r.replace(os.sep, os.sep + os.sep)
+    replace_in_file(os.path.join(b, 'lib/python2.7/site-packages/sipconfig.py'), q, r)
--- scripts/pkgs/utils.py
+++ scripts/pkgs/utils.py
@@ -180,7 +180,7 @@ def replace_in_file(path, old, new, missing_ok=False):
         else:
             nraw = old.sub(new, raw)
         if raw == nraw and not missing_ok:
-            raise SystemExit('Failed to patch: ' + path)
+            raise ValueError('Failed to patch: ' + path)
         f.seek(0), f.truncate()
         f.write(nraw)
 
