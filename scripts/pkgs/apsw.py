86972f0d2911ed7c1f4cd5ac208fc3a3ce937d7e
Kovid Goyal <kovid@kovidgoyal.net>
2016-09-13 13:18:16 +0530

    Fix sqlite linking in apsw on OS X

--- scripts/pkgs/apsw.py
+++ scripts/pkgs/apsw.py
@@ -16,3 +16,14 @@ if iswindows:
         run(PYTHON, 'setup.py', 'fetch', '--all', '--missing-checksum-ok', 'build', 'install', '--root', build_dir())
         os.rename(os.path.join(build_dir(), os.path.basename(SW), os.path.basename(PREFIX), 'private'), os.path.join(build_dir(), 'private'))
         shutil.rmtree(os.path.join(build_dir(), os.path.basename(SW)))
+
+
+def install_name_change_predicate(x):
+    return x.endswith('apsw.so')
+
+
+def install_name_change(old_name, is_dep):
+    bn = os.path.basename(old_name)
+    if bn.startswith('libsqlite'):
+        return os.path.join(PREFIX, 'lib', bn)
+    return old_name
