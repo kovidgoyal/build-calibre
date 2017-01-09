1964e1493c6168442c1ef670131e8d9ed7b2f1d8
Kovid Goyal <kovid@kovidgoyal.net>
2017-01-09 21:56:24 +0530

    Build glew

---
+++ scripts/pkgs/glew.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
+
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+import os
+import re
+
+from .constants import build_dir, PREFIX
+from .utils import run, replace_in_file
+
+
+def main(args):
+    run('make GLEW_DEST={} install'.format(build_dir()))
+    replace_in_file(os.path.join(build_dir(), 'lib/pkgconfig/glew.pc'), re.compile(br'^prefix=.+$', re.M), b'prefix=%s' % PREFIX)
+    replace_in_file(os.path.join(build_dir(), 'lib/pkgconfig/glew.pc'), re.compile(br'^libdir=.+$', re.M), b'libdir=${prefix}/lib')
