aac66d92f10fa642b49c0270324909564ff1bf30
Kovid Goyal <kovid@kovidgoyal.net>
2017-09-22 10:00:55 +0530

    Update version of expat

--- scripts/pkgs/expat.py
+++ scripts/pkgs/expat.py
@@ -4,17 +4,13 @@
 
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
-import re
-
 from .constants import iswindows
-from .utils import windows_cmake_build, replace_in_file
+from .utils import windows_cmake_build
 
 
 if iswindows:
     def main(args):
-        # This command causes nmake to fail and we dont want the docs anyway
-        replace_in_file('CMakeLists.txt', re.compile('^add_custom_command.+ doc .+$', re.MULTILINE), '')
         windows_cmake_build(
-            BUILD_tools='OFF', BUILD_examples='OFF', BUILD_tests='OFF',
+            BUILD_tools='OFF', BUILD_examples='OFF', BUILD_tests='OFF', BUILD_doc='OFF',
             binaries='expat.dll', libraries='expat.lib', headers='../lib/expat.h ../lib/expat_external.h'
         )
