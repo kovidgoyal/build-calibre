3d30502cd7b069c63cab91586f4dbf95c0c1fd7d
Kovid Goyal <kovid@kovidgoyal.net>
2017-11-10 16:18:41 +0530

    Harbuzz breaks if built with core text

--- scripts/pkgs/harfbuzz.py
+++ scripts/pkgs/harfbuzz.py
@@ -8,15 +8,14 @@ from __future__ import (absolute_import, division, print_function,
 import os
 import re
 
-from .constants import PREFIX, build_dir, isosx
+from .constants import PREFIX, build_dir
 from .utils import replace_in_file, simple_build
 
 
 def main(args):
     simple_build(
         '--disable-dependency-tracking --disable-static --with-glib=no --with-freetype=yes'
-        ' --with-gobject=no --with-cairo=no --with-fontconfig=no --with-icu=no' + (
-            ' --with-coretext=' + ('yes' if isosx else 'no'))
+        ' --with-gobject=no --with-cairo=no --with-fontconfig=no --with-icu=no'
     )
     pc = os.path.join(build_dir(), 'lib/pkgconfig/harfbuzz.pc')
     replace_in_file(pc, re.compile(br'^prefix=.+$', re.M), b'prefix=%s' % PREFIX)
